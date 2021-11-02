075b4ddbd8b76123a9fee4c84d2322ef66caa0c2
Blankman <60937724+jkblank@users.noreply.github.com>
2021-11-02 15:29:46 +0200

    Update app.js
    CORS Changes

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(function(req,res,next){
+  res.header("Access-Control-Allow_Origin", "https://www.postalot-client.herokuapp.com");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+}); 
 app.use(cors());
 
 const db = knex({
